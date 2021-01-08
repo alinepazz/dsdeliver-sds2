7423c0df756cf78b73b2cdf29f3d19f5069d5764
Aline Paz <aline.soares.2@hotmail.com>
2021-01-08 06:43:47 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
+import { Route, Router } from 'react-router-dom';
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <Routes />
   );
 }
-
 export default App;
