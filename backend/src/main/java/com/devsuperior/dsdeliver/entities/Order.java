e5a722b1a2fe15e5527b378c3794ca8c80a47943
Aline Paz <aline.soares.2@hotmail.com>
2021-01-11 18:53:08 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -35,6 +35,7 @@ public class Order implements Serializable{
 		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
 	
+	
 	public Order() {
 	}
 
@@ -94,6 +95,14 @@ public class Order implements Serializable{
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
 	}
 
 	public Set<Product> getProducts() {
