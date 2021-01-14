6088cb17986b73e971096bda6ee1fd98fc452f02
Aline Paz <aline.soares.2@hotmail.com>
2021-01-14 19:12:32 -0300

    Ajustes finais

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,3 +1,4 @@
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, ScrollView, Alert, Text} from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
@@ -10,14 +11,28 @@ import { Order } from '../types';
  function Orders() {
   const [orders, setOrders] = useState<Order[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+const fetchData = () => {
+  setIsLoading(true);
+  fetchOrders()
+  .then(response =>setOrders(response.data))
+  .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+  .finally(() => setIsLoading(false));
+}
 
   useEffect(() =>{
-    setIsLoading(true);
-    fetchOrders()
-    .then(response =>setOrders(response.data))
-    .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
-    .finally(() => setIsLoading(false));
-  }, []);
+    if (isFocused){
+      fetchData();
+    }
+  }, [isFocused]);
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate('OrderDetails', {
+      order
+    });
+  }
 
   return (
    <>
@@ -27,7 +42,10 @@ import { Order } from '../types';
          <Text>Buscando Pedidos...</Text>
        ) : (
         orders.map(order => (
-          <TouchableWithoutFeedback key={order.id}>
+          <TouchableWithoutFeedback
+           key={order.id} 
+           onPress={() => handleOnPress(order)}
+           >
              <OrderCard order={order} />
           </TouchableWithoutFeedback>
         ))
--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -5,3 +5,7 @@ const API_URL = 'https://aline-sds2-nelio.herokuapp.com';
 export function fetchOrders(){
     return axios(`${API_URL}/orders`)
 }
+
+export function confirmDelivery(orderId: number) {
+    return axios.put(`${API_URL}/orders/${orderId}/delivered`)
+}
